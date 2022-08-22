3869ec7fdfb745c80ff7e46b8d124d04c379a7fe
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-08-22 12:35:26 +0200

    list creation added2

---
+++ index.js
@@ -0,0 +1,40 @@
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+
+let lists = []
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value
+    if(listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name){
+    return {id: Date.now().toString(), name, tasks: []}
+}
+
+function render(){
+    clearElement(listsContainer)
+
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add("task")
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement);
+    })
+}
+
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
+}
+
+render();
